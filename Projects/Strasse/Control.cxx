44f7ab75c822a75e1620f4aebfa9a293f8dcf462
adrien matta <matta@lpccaen.in2p3.fr>
2020-07-20 17:23:03 +0200

    * Strasse outer barrel analysis

--- Projects/Strasse/Control.cxx
+++ Projects/Strasse/Control.cxx
@@ -1,33 +1,39 @@
 void Control(){
   TFile* file = TFile::Open("../../Outputs/Analysis/PhysicsTree.root");
   TTree* PhysicsTree= (TTree*) file->FindObjectAny("PhysicsTree");
-  TCanvas* c = new TCanvas("Control","Control",1000,1000);
-  c->Divide(2,2);
-  //string cond = "InnerPosX!=-60 && InnerStripL[0]==64  && InnerStripT[0]==152";
-
-  string cond = "InnerPosX!=-60 && (DetectorNumber==3 || DetectorNumber==1)";
-  c->cd(1);
-//  PhysicsTree->Draw("InnerPosX:fDetected_Position_X",cond.c_str(),"col") ; 
-//
-  PhysicsTree->Draw("InnerPosX-fDetected_Position_X>>h(1000,-2,2)",cond.c_str(),"col") ; 
-  c->cd(2);
-  //PhysicsTree->Draw("InnerPosY:fDetected_Position_Y",cond.c_str(),"col") ; 
-
-  PhysicsTree->Draw("InnerPosY-fDetected_Position_Y>>h2(1000,-2,2)",cond.c_str(),"col") ; 
-  c->cd(3);
-  //PhysicsTree->Draw("InnerPosZ:fDetected_Position_Z",cond.c_str(),"col") ; 
-
-  PhysicsTree->Draw("InnerPosZ-fDetected_Position_Z>>h3(1000,-2,2)",cond.c_str(),"col") ; 
-  c->cd(3);
-  c->cd(4);
+  string cond = "InnerPosX!=-1000";
+/*
+  TCanvas* cInner = new TCanvas("ControlInner","ControlInner",1000,1000);
+  cInner->Divide(2,2);
+  cInner->cd(1);
+  PhysicsTree->Draw("InnerPosX:fDetected_Position_X",cond.c_str(),"col") ; 
+  cInner->cd(2);
+  PhysicsTree->Draw("InnerPosY:fDetected_Position_Y",cond.c_str(),"col") ; 
+  cInner->cd(3);
+  PhysicsTree->Draw("InnerPosZ:fDetected_Position_Z",cond.c_str(),"col") ; 
+  cInner->cd(4);
   PhysicsTree->Draw("InnerPosY:InnerPosX:InnerPosZ", cond.c_str(),"");            
+*/
+  TCanvas* cOuter = new TCanvas("ControlOuter","ControlOuter",1000,1000);
+  cOuter->Divide(2,2);
+  cond = "OuterPosX!=-1000";
+  cOuter->cd(1);
+  PhysicsTree->Draw("OuterPosX:fDetected_Position_X[3]",cond.c_str(),"col") ; 
+  cOuter->cd(2);
+  PhysicsTree->Draw("OuterPosY:fDetected_Position_Y[3]",cond.c_str(),"col") ; 
+  cOuter->cd(3);
+  PhysicsTree->Draw("OuterPosZ:fDetected_Position_Z[3]",cond.c_str(),"col") ; 
+  cOuter->cd(4);
+  PhysicsTree->Draw("OuterPosY:OuterPosX:OuterPosZ", cond.c_str(),"");            
 
- /* TCanvas* c2 = new TCanvas("Control 2", "Control2",500,500,2000,1000);  
+
+  TCanvas* c2 = new TCanvas("Control 2", "Control2",500,500,2000,1000);  
   c2->Divide(2,1);
   c2->cd(1);
-  PhysicsTree->Draw("InnerPosY:InnerPosX","InnerPosX!=-60");            
+  PhysicsTree->Draw("OuterPosY:OuterPosX",cond.c_str());            
+  PhysicsTree->Draw("InnerPosY:InnerPosX",cond.c_str(),"same");            
 
   c2->cd(2);
-  PhysicsTree->Draw("fDetected_Position_Y:fDetected_Position_X","InnerPosX!=-60","");            
-*/
+  PhysicsTree->Draw("OuterPosY:OuterPosZ",cond.c_str());            
+  PhysicsTree->Draw("InnerPosY:InnerPosZ",cond.c_str(),"same");            
 }
