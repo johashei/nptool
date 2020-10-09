5eda9bf94c7d5de5ecfffadeb5f801689d5e3faf
morfouacep <morfouacep@u-un00307333>
2020-10-09 15:01:05 +0200

    Modifying NPDetectorManager for NON-THREADING mode

--- NPLib/Core/NPDetectorManager.cxx
+++ NPLib/Core/NPDetectorManager.cxx
@@ -260,7 +260,6 @@ void NPL::DetectorManager::BuildPhysicalEvent(){
   static std::map<std::string,VDetector*>::iterator end= m_Detector.end();
 
   for (it =begin; it != end; ++it) {
-    cout << " " <<endl;
     (it->second->*m_ClearEventPhysicsPtr)();
     (it->second->*m_BuildPhysicalPtr)();
     if(m_FillSpectra){
