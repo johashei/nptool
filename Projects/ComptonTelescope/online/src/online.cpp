d56b2774709f451e5251cf5b5c0de65339da328d
gypaos <gypaos@gmail.com>
2020-10-18 23:03:19 +0200

    debugging online.cpp compilation

--- Projects/ComptonTelescope/online/src/online.cpp
+++ Projects/ComptonTelescope/online/src/online.cpp
@@ -1,12 +1,11 @@
 // nptool headers
-//#include "NPOptionManager.h"
-//#include "RootOutput.h"
-//#include "NPDetectorManager.h"
-//#include "TComptonTelescopeData.h"
-//#include "TComptonTelescopePhysics.h"
+#include "NPOptionManager.h"
+#include "RootOutput.h"
+#include "NPDetectorManager.h"
+#include "TComptonTelescopeData.h"
+#include "TComptonTelescopePhysics.h"
 
 // root headers
-#include "TH1F.h"
 
 // custom headers
 #include "DecodeR.h"
@@ -18,49 +17,50 @@
 #include <vector>
 using namespace std;
 
+
 int main()
 {
-   auto pipo = new TH1F("h1", "h1", 100, 0, 100);
-   pipo->Print();
-   pipo->Dump();
-
    ///////////////////////////////////////////////////////////////////////////
    // configure option manager
-//   NPOptionManager::getInstance()->Destroy();
+   NPOptionManager::getInstance()->Destroy();
 
 //   char arg[1000];
 //   sprintf(arg,"-D ./ComptonCAM.detector -C Calibration.txt -GH -E Example2.reaction -P %i --circular",port);
 //   sprintf(arg,"-D ./ComptonCAM.detector -C calibrations.txt -GH -E Example2.reaction --circular");
 //   sprintf(arg,"-D ./ComptonCAM.detector -C calibrations.txt -GH -E ./10He.reaction --circular");
-/*   string arg = "-D ./ComptonCAM.detector -C calibrations.txt -GH -E ./10He.reaction --circular";
+   string arg = "-D ./ComptonCAM.detector -C calibrations.txt -GH -E ./10He.reaction --circular";
    NPOptionManager::getInstance(arg);  
 
    // ROOT output file name
-   RootOutput::getInstance("OnlineTree.root", "OnlineTree");
+// RootOutput::getInstance("OnlineTree.root", "OnlineTree");
 
    // configure detector manager
    string detectorfileName = NPOptionManager::getInstance()->GetDetectorFile();
    cout << "detector file name from NPOptionManager: " << detectorfileName << "\n";
    NPL::DetectorManager* m_NPDetectorManager = new NPL::DetectorManager();
-   m_NPDetectorManager->ReadConfigurationFile(detectorfileName);*/
-/*   m_NPDetectorManager->InitializeRootOutput();*/
+//   m_NPDetectorManager->ReadConfigurationFile(detectorfileName);
+//   m_NPDetectorManager->InitializeRootOutput();
 
+   ///////////////////////////////////////////////////////////////////////////
+   // this part is commented for debugging purposes, but it works on its own
+   ///////////////////////////////////////////////////////////////////////////
    // instantiate raw ComptonCAM data pointer
-//   auto ccamData = new TComptonTelescopeData();
+/*   auto ccamData = new TComptonTelescopeData();
+   ccamData->Dump();
    // connect raw CCAM data pointer to physics class
-/*   auto ccamPhys = (TComptonTelescopePhysics*) m_NPDetectorManager->GetDetector("ComptonTelescope");
-   ccamPhys->SetRawDataPointer(ccamData);*/
+//   auto ccamPhys = (TComptonTelescopePhysics*) m_NPDetectorManager->GetDetector("ComptonTelescope");
+//   ccamPhys->SetRawDataPointer(ccamData);
    
    // read data file/flux and fill ccamData object
-
-   cout << "Reading data" << endl;
+   std::cout << "Reading data\n";
+   // instantiate DecodeR object reading calorimeter data flux
    DecodeR* D = new DecodeR(false);
    D->Dump();
 
    // Load a file
-   ifstream is;
-   is.open("../mfm.bin", ios::binary);
-   is.seekg (0, ios::end);
+   std::ifstream is;
+   is.open("../mfm.bin", std::ios::binary);
+   is.seekg (0, std::ios::end);
    int length = is.tellg();
    is.seekg (0, ios::beg);
    char* buffer = new char [length];
@@ -69,14 +69,14 @@ int main()
 
    // Read from file
    D -> setRaw(buffer);
-   D -> decodeRawMFM();//Get rid of the first two (empty) events
+   D -> decodeRawMFM(); // get rid of the first two (empty) events
    D -> decodeRawMFM();
    D -> decodeRawMFM();
    D -> Dump();
 
-/*
+
    int c = 0;
-   int i = 0;
+//   int i = 0;
 
    while (D -> getCursor() < length)
    {
@@ -98,10 +98,10 @@ int main()
       ccamData -> Dump();
       ccamData -> Clear();
       c++;
-   }*/
+   }
    delete D;
    delete [] buffer;
-
+*/
    // test zone...
 /*
    ccamData->SetCTTrackerFrontETowerNbr(1);
