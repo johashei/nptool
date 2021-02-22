e96d7708f9d3f0b4592a4dd6c303aba2018660a4
Warren Lynch <warren.lynch@york.ac.uk>
2021-02-22 13:04:22 +0000

    works again for particle source

--- NPSimulation/Detectors/TACTIC/TACTICScorer.cc
+++ NPSimulation/Detectors/TACTIC/TACTICScorer.cc
@@ -2,6 +2,7 @@
 
 #include "TACTICScorer.hh"
 #include "G4UnitsTable.hh"
+#include "G4RunManager.hh"
 
 #ifdef USE_Garfield
 #include "GARFDRIFT.h"
@@ -24,31 +25,33 @@ Gas_Scorer::~Gas_Scorer(){}
 
 G4bool Gas_Scorer::ProcessHits(G4Step* aStep, G4TouchableHistory*){
 
-  G4double* Infos = new G4double[12];
+  G4double* Infos = new G4double[13];
   m_Position  = aStep->GetPreStepPoint()->GetPosition();
 
-  Infos[0] = aStep->GetTrack()->GetTrackID();
+  Infos[0] = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
 
-  Infos[1] = aStep->GetTrack()->GetParticleDefinition()->GetAtomicNumber();;
+  Infos[1] = aStep->GetTrack()->GetTrackID();
+
+  Infos[2] = aStep->GetTrack()->GetParticleDefinition()->GetAtomicNumber();;
   
-  Infos[2] = aStep->GetPreStepPoint()->GetGlobalTime();
-  Infos[3] = aStep->GetPreStepPoint()->GetKineticEnergy();
-  Infos[4] = aStep->GetTotalEnergyDeposit() - aStep->GetNonIonizingEnergyDeposit();
+  Infos[3] = aStep->GetPreStepPoint()->GetGlobalTime();
+  Infos[4] = aStep->GetPreStepPoint()->GetKineticEnergy();
+  Infos[5] = aStep->GetTotalEnergyDeposit() - aStep->GetNonIonizingEnergyDeposit();
 
   m_SegmentNumber = (int)((m_Position.z() + m_ScorerLength / 2.) / m_SegmentLength ) + 1; //Pad number 
-  Infos[5] = m_SegmentNumber;
-  Infos[6] = m_Position.z();
-  Infos[7] = pow(pow(m_Position.x(),2) + pow(m_Position.y(),2),0.5); //R
-  Infos[8] = aStep->GetTrack()->GetVertexPosition()[2];
-  Infos[9] = aStep->GetTrack()->GetVertexKineticEnergy();
+  Infos[6] = m_SegmentNumber;
+  Infos[7] = m_Position.z();
+  Infos[8] = pow(pow(m_Position.x(),2) + pow(m_Position.y(),2),0.5); //R
+  Infos[9] = aStep->GetTrack()->GetVertexPosition()[2];
+  Infos[10] = aStep->GetTrack()->GetVertexKineticEnergy();
   G4ThreeVector p_vec = aStep->GetTrack()->GetVertexMomentumDirection();
-  Infos[10] = acos(p_vec[2]/pow(pow(p_vec[0],2)+pow(p_vec[1],2)+pow(p_vec[2],2),0.5))/deg; //angle relative to z axis (theta);   
-  Infos[11] = aStep->GetTrack()->GetTrackLength();
-
+  Infos[11] = acos(p_vec[2]/pow(pow(p_vec[0],2)+pow(p_vec[1],2)+pow(p_vec[2],2),0.5))/deg; //angle relative to z axis (theta);   
+  Infos[12] = aStep->GetTrack()->GetTrackLength();
+  
   m_DetectorNumber = aStep->GetPreStepPoint()->GetTouchableHandle()->GetCopyNumber(m_Level);
   m_Index = m_DetectorNumber * 1e3 + m_SegmentNumber * 1e6;
   
-  if(isnan(Infos[9])) {
+  if(isnan(Infos[10])) {
     aStep->GetTrack()->SetTrackStatus(fStopAndKill);
     return 0;
   }
@@ -56,7 +59,7 @@ G4bool Gas_Scorer::ProcessHits(G4Step* aStep, G4TouchableHistory*){
 #ifdef USE_Garfield
   G4ThreeVector delta_Position = aStep->GetDeltaPosition();
 
-  GARFDRIFT(Infos[4]/eV, Infos[2], m_Position/cm, delta_Position/cm, Infos[7]/cm, Infos[5], Infos[1], m_ScorerLength/cm, m_SegmentLength/cm);
+  GARFDRIFT(Infos[5]/eV, Infos[3], m_Position/cm, delta_Position/cm, Infos[8]/cm, Infos[6], Infos[2], m_ScorerLength/cm, m_SegmentLength/cm, Infos[0], Infos[2], Infos[11]);
 #endif
   
   map<G4int, G4double**>::iterator it;
