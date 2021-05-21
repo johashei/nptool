4a2cc574d723fe152f6c9030918532edc5848e37
Adrien Matta <matta@lpccaen.in2p3.fr>
2021-05-21 15:55:41 +0200

    * Improving CheckMinos macro

--- Projects/S034/macro/CheckMinos.sh
+++ Projects/S034/macro/CheckMinos.sh
@@ -1,9 +1,12 @@
 # convert physics run
-ridf2nptool raw/run0582.ridf.gz root/mrdc/run582.root db/s034_list.txt 
+ridf2nptool -l raw/run0582.ridf.gz root/mrdc/run582.root db/s034_list.txt 
 # convert an empty target run
-ridf2nptool raw/run0824.ridf.gz root/mrdc/run824.root db/s034_list.txt 
+ridf2nptool -l raw/run0824.ridf.gz root/mrdc/run824.root db/s034_list.txt 
+# convert another physics run
+ridf2nptool -l raw/run0570.ridf.gz root/mrdc/run570.root db/s034_list.txt 
 # analysis of both run
-npcompilation -l && npanalysis -D s034.detector -C calibration.txt -T root/mrdc/run582.root RawTree -O run582CheckMinos -L 100000
-npcompilation -l && npanalysis -D s034.detector -C calibration.txt -T root/mrdc/run824.root RawTree -O run824CheckMinos -L 100000
+npcompilation -l && npanalysis -D s034.detector  -C calibration.txt -T root/mrdc/run582.root RawTree -O run582CheckMinos 
+npcompilation -l && npanalysis -D s034.detector  -C calibration.txt -T root/mrdc/run824.root RawTree -O run824CheckMinos 
+npcompilation -l && npanalysis -D s034.detector  -C calibration.txt -T root/mrdc/run570.root RawTree -O run570CheckMinos 
 # Plot the results
 root macro/CheckMinos.cxx
