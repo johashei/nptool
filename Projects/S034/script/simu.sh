b75adbd95ed709562e77d00777efd23daf8ec179
Adrien Matta <matta@lpccaen.in2p3.fr>
2021-10-15 08:56:45 +0200

    * fixing issue in S034 scripts

--- Projects/S034/script/simu.sh
+++ Projects/S034/script/simu.sh
@@ -2,5 +2,5 @@
 
 for i in {1..20}
 do
-  nohup npsimulation -D minos.detector -E reaction/pp.reaction -B run.mac -O simu_tpad_$i --random-seed $i & 
+  nohup npsimulation -D minos_dali.detector -E reaction/pp.reaction -B run.mac -O simu_tpad_$i --random-seed $i & 
 done
