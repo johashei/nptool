b454f8f7d2b2e0a8500445fcd772123bf7ee26f7
Elia Pilotto <pilottoelia@gmail.com>
2021-11-29 16:04:08 +0100

    Changes in root macro for analysis

--- Projects/Samurai/benchmarkMacro.C
+++ Projects/Samurai/benchmarkMacro.C
@@ -1,11 +1,26 @@
 
 void xy_graph(vector<double> x, vector <double> y);
 
+double mean(vector<double>V){
+    double s = 0;
+    for (auto x:V) s += x;
+    return s / V.size();
+}
+double var(vector<double>V){
+    double s = 0, m=mean(V);
+    for (auto x:V) s += (x-m)*(x-m);
+    return s / (V.size()-1);
+}
+
+
 void benchmarkMacro (){
     int n_file = 20;
+    string path = "root/simulation/bm1/";
+    string set = "EO/spm3/";
+    cout << "Read from " << path + set << endl;
     TChain* chain = new TChain("SimulatedTree");
     for (auto i=0; i<n_file; i++){
-        string filename = "root/simulation/benchmark" + to_string(i+1) + ".root";
+        string filename = path + set + "benchmark" + to_string(i+1) + ".root";
         chain->Add(filename.c_str());
     }
 
@@ -22,29 +37,41 @@ void benchmarkMacro (){
     int entries = chain->GetEntries();
     for (int i=0; i<entries; i++){
         chain->GetEntry(i);
-        for (auto j=0;j<Magnet->GetMult(); j++){
-            X.push_back(-Magnet->GetPosX(j));
-            //Y.push_back(FDC2->GetPosY(j));
-            Y.push_back(Magnet->GetPosZ(j));
+        for (auto j=0;j<FDC2->GetMult(); j++){
+            X.push_back(FDC2->GetPosX(j));
+            Y.push_back(FDC2->GetPosY(j));
+            //Y.push_back(FDC2->GetPosZ(j));
             //Y.push_back(FDC2->GetBrho(j));
         }
 
     }
 
-    double ylim = 20, xa=-3812, xb=-3794;
-    TH2D* h2d = new TH2D("h2d", "X:Y", 100, xa, xb, 100, -ylim, ylim);
-    //TH2D* h2d = new TH2D("h2d", "X:Brho", 100, -3812, -3794, 100, 6.06, 6.12);
-    
-    for (int i=0; i<X.size(); i++){
-        h2d->Fill(X[i], Y[i]);
-    }
+    TCanvas* tc = new TCanvas();
+
+    TH1D* h1d = new TH1D("h1d", "X at FDC2", 100, -3812, -3794);
+    for (auto i=0; i<X.size(); i++) h1d->Fill(X[i]);
+    h1d->Draw();
+    tc->Print((path+set+"histoX_FDC2.pdf").c_str());
+
+    TH2D* h2d = new TH2D("h2d", "X:Y", 100, -3812, -3794, 100, -20, 20);
+    for (int i=0; i<X.size(); i++) h2d->Fill(X[i], Y[i]);
     h2d->Draw("colz");
-    xy_graph(X, Y);
+    tc->Print((path+set+"histoXY_FDC2.pdf").c_str());
+
+    cout << "X\n";
+    cout << "mean = " << mean(X) << endl;
+    cout << "std dev = " << sqrt(var(X)) << endl;
+    cout << "Y\n";
+    cout << "mean = " << mean(Y) << endl;
+    cout << "std dev = " << sqrt(var(Y)) << endl;
+    
 
 }
 
 //-------------------------------------------------------------
 
+
+//-------------------------------------------------------------
 void t_hist(vector<double> t){
 
     TH1D* hist = new TH1D("h1", "h1 title", 50, 0, 1);
