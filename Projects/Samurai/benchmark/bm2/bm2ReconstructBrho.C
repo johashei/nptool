af2b447635704bc10b347f40f6d7d287fd2f13fb
Elia Pilotto <pilottoelia@gmail.com>
2022-01-10 11:33:01 +0100

    fixing brho reconstruction

--- Projects/Samurai/benchmark/bm2/bm2ReconstructBrho.C
+++ Projects/Samurai/benchmark/bm2/bm2ReconstructBrho.C
@@ -1,4 +1,3 @@
-
 void Brhos(string path_to_file);
 
 void bm2ReconstructBrho (){
@@ -8,7 +7,7 @@ void bm2ReconstructBrho (){
     //vector<string> paths ={"field_10mm/1x5y/", "field_20mm/"};
     //for(auto p:paths) Brhos(p);
 
-    //Brhos("field_10mm/1x5y/");
+    Brhos("resolution/1cm_E480/");
     //Brhos("field_10mm/5x1y/");
     //Brhos("field_20mm/");
     //Brhos("field_10mm/spm1000/1cm_E0/");
@@ -65,16 +64,17 @@ void Brhos(string path_to_file){
     TVector3 Mag_Pos (0,0,10000*mm);
     TVector3 Pos_FDC1, Dir_FDC1, Pos_FDC2, Dir_FDC2;
     double fx1, fx2, fy1, fy2;
-    static const Res_FDC1 = 110*um, Res_FDC2 = 120*um;
+    static const double Res_FDC1 = 110*um, Res_FDC2 = 120*um;
+    static TRandom2* Rand_Generator = new TRandom2 ();
     for (int i=0; i<entries; i++){
         cout << i << " " << path_to_file  << " ";
         for (auto j=0; j<time.size(); j++) br_recs[j]=-1;
         input->GetEntry(i);
         if (FDC2->GetMult() == 1 && FDC1->GetMult() == 1){
-            fx1 = G4RandGauss::shoot(FDC1->GetPosX(0), Res_FDC1);
-            fy1 = G4RandGauss::shoot(FDC1->GetPosY(0), Res_FDC1);
-            fx2 = G4RandGauss::shoot(FDC2->GetPosX(0), Res_FDC2);
-            fy2 = G4RandGauss::shoot(FDC2->GetPosY(0), Res_FDC2);
+            fx1 = Rand_Generator->Gaus(FDC1->GetPosX(0), Res_FDC1);
+            fy1 = Rand_Generator->Gaus(FDC1->GetPosY(0), Res_FDC1);
+            fx2 = Rand_Generator->Gaus(FDC2->GetPosX(0), Res_FDC2);
+            fy2 = Rand_Generator->Gaus(FDC2->GetPosY(0), Res_FDC2);
             Pos_FDC1 = TVector3(fx1, fy1, FDC1->GetPosZ(0)) - Mag_Pos;
             Pos_FDC2 = TVector3(fx2, fy2, FDC2->GetPosZ(0)) - Mag_Pos;
             Dir_FDC1.SetMagThetaPhi(FDC1->GetMomMag(0), FDC1->GetMomTheta(0), FDC1->GetMomPhi(0));
