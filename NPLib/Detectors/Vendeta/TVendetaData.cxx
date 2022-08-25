91650f3805931ec7a4ab7b6a03389f5404fd7e69
Cyril Lenain <lenain@lpccaen.in2p3.fr>
2022-08-25 10:21:27 -0600

    Adding saturation flag in Vendeta

--- NPLib/Detectors/Vendeta/TVendetaData.cxx
+++ NPLib/Detectors/Vendeta/TVendetaData.cxx
@@ -50,12 +50,14 @@ void TVendetaData::Clear() {
   fVendeta_LG_Q2.clear();
   fVendeta_LG_Time.clear();
   fVendeta_LG_Qmax.clear();
+  fVendeta_LG_IsSat.clear();
  
   fVendeta_HG_DetectorNbr.clear();
   fVendeta_HG_Q1.clear();
   fVendeta_HG_Q2.clear();
   fVendeta_HG_Time.clear();
   fVendeta_HG_Qmax.clear();
+  fVendeta_HG_IsSat.clear();
 
 }
 
--- NPLib/Detectors/Vendeta/TVendetaData.h
+++ NPLib/Detectors/Vendeta/TVendetaData.h
@@ -40,12 +40,14 @@ class TVendetaData : public TObject {
     vector<Double_t>   fVendeta_LG_Q2;
     vector<Double_t>   fVendeta_LG_Time;
     vector<Double_t>   fVendeta_LG_Qmax; 
+    vector<bool>       fVendeta_LG_IsSat; 
 
     vector<UShort_t>   fVendeta_HG_DetectorNbr;
     vector<Double_t>   fVendeta_HG_Q1;
     vector<Double_t>   fVendeta_HG_Q2;
     vector<Double_t>   fVendeta_HG_Time;
     vector<Double_t>   fVendeta_HG_Qmax; 
+    vector<bool>       fVendeta_HG_IsSat; 
 
     //////////////////////////////////////////////////////////////
     // Constructor and destructor
@@ -74,12 +76,14 @@ class TVendetaData : public TObject {
     inline void SetLGQ2(const Double_t& Q2) {fVendeta_LG_Q2.push_back(Q2);};//!
     inline void SetLGTime(const Double_t& Time) {fVendeta_LG_Time.push_back(Time);};//!
     inline void SetLGQmax(const Double_t& Qmax) {fVendeta_LG_Qmax.push_back(Qmax);};//
+    inline void SetLGIsSat(const bool& IsSat) {fVendeta_LG_IsSat.push_back(IsSat);};//
 
     inline void SetHGDetectorNbr(const UShort_t& DetNbr) {fVendeta_HG_DetectorNbr.push_back(DetNbr);};//!
     inline void SetHGQ1(const Double_t& Q1) {fVendeta_HG_Q1.push_back(Q1);};//!
     inline void SetHGQ2(const Double_t& Q2) {fVendeta_HG_Q2.push_back(Q2);};//!
     inline void SetHGTime(const Double_t& Time) {fVendeta_HG_Time.push_back(Time);};//!
     inline void SetHGQmax(const Double_t& Qmax) {fVendeta_HG_Qmax.push_back(Qmax);};//
+    inline void SetHGIsSat(const bool& IsSat) {fVendeta_HG_IsSat.push_back(IsSat);};//
 
     //////////////////////    GETTERS    ////////////////////////
     inline UShort_t GetLGMultEnergy() const {return fVendeta_LG_DetectorNbr.size();}
@@ -88,6 +92,7 @@ class TVendetaData : public TObject {
     inline Double_t GetLGQ2(const unsigned int &i) const {return fVendeta_LG_Q2[i];}//!
     inline Double_t GetLGTime(const unsigned int &i) const {return fVendeta_LG_Time[i];}//!
     inline Double_t GetLGQmax(const unsigned int &i) const {return fVendeta_LG_Qmax[i];}//!
+    inline bool     GetLGIsSat(const unsigned int &i) const {return fVendeta_LG_IsSat[i];}//!
 
     inline UShort_t GetHGMultEnergy() const {return fVendeta_HG_DetectorNbr.size();}
     inline UShort_t GetHGDetectorNbr(const unsigned int &i) const {return fVendeta_HG_DetectorNbr[i];}//!
@@ -95,6 +100,7 @@ class TVendetaData : public TObject {
     inline Double_t GetHGQ2(const unsigned int &i) const {return fVendeta_HG_Q2[i];}//!
     inline Double_t GetHGTime(const unsigned int &i) const {return fVendeta_HG_Time[i];}//!
     inline Double_t GetHGQmax(const unsigned int &i) const {return fVendeta_HG_Qmax[i];}//!
+    inline bool     GetHGIsSat(const unsigned int &i) const {return fVendeta_HG_IsSat[i];}//!
 
     //////////////////////////////////////////////////////////////
     // Required for ROOT dictionnary
